9bbdcab84636ea00c34bf200cab0d3d1df1cba65
Gogs <gogs@fake.local>
2016-10-16 14:57:37 +1100

    x

---
+++ cURL/post-facts.sh
@@ -0,0 +1,2 @@
+
+curl -i -H "Content-Type: application/xml" -H "X-KIE-ContentType: XSTREAM" -d @"post-facts.xml" -u "kieserver:kieserver1!" -X POST "http://weightwatcherrules-weightwatcher.127.0.0.1.xip.io/kie-server/services/rest/server/containers/instances/watch"
---
+++ cURL/stop-scanner.sh
@@ -0,0 +1,2 @@
+
+curl -i -H "Content-Type: application/xml" -d @"start-scanner.xml" -u "erics:jbossbrms1!" -X POST "http://weightwatcher.cloudapps.example.com/kie-server/services/rest/server/containers/watch/scanner"
