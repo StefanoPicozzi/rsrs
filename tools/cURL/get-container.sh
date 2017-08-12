f7a1fb6d0c20a381afbf54a22c177d91d119e5d8
Gogs <gogs@fake.local>
2017-08-12 15:47:01 +1000

    x

--- tools/cURL/get-container.sh
+++ tools/cURL/get-container.sh
@@ -1,1 +1,1 @@
-curl --user "kieserver:kieserver1!" "http://weightwatcher-weightwatcher.127.0.0.1.xip.io/kie-server/services/rest/server/containers/watch"
+curl --user "kieserver:kieserver1!" "http://weightwatcher-weightwatcher.127.0.0.1.nip.io/kie-server/services/rest/server/containers/watch"
